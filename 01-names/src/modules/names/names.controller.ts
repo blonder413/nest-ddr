42e87cfc5d40e6b0b40fe95daf6f2b54b1e61de1
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-11-15 15:43:54 -0500

    decorador @ApiOperation

--- 01-names/src/modules/names/names.controller.ts
+++ 01-names/src/modules/names/names.controller.ts
@@ -9,7 +9,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { NamesService } from './names.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('api/v1/names')
 @ApiTags('names')
@@ -17,26 +17,49 @@ export class NamesController {
   constructor(private namesService: NamesService) {}
 
   @Post()
+  @ApiOperation({
+    description:
+      'Crea un nuevo nombre. Retorna true si se inserta correctamente',
+  })
   createName(@Body() data: { name: string }) {
     return this.namesService.createName(data.name);
   }
 
   @Get()
+  @ApiOperation({
+    description:
+      'Devuelve todos los nombres que inician con una cadena de texto dadaa',
+  })
   getNames(@Query('start') start: string) {
     return this.namesService.getNames(start);
   }
 
   @Put('/:name/:newName')
+
+  @ApiOperation({
+    description:
+      'Actualiza un nuevo nombre',
+  })
   updateName(@Param('name') name: string, @Param('newName') newName: string) {
     return this.namesService.updateName(name, newName);
   }
 
   @Delete('clear')
+
+  @ApiOperation({
+    description:
+      'Elimina todos los nombres',
+  })
   clearNames() {
     return this.namesService.clearNames();
   }
 
   @Delete('/:name')
+
+  @ApiOperation({
+    description:
+      'Elimina el nombre dado',
+  })
   deleteName(@Param('name') name: string) {
     return this.namesService.deleteName(name);
   }
