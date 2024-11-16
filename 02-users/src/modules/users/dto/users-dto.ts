128b163e60ea6700e33dfefe780a93d6635e3c69
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-11-16 16:14:10 -0500

    api user: validación de entrada de datos

--- 02-users/src/modules/users/dto/users-dto.ts
+++ 02-users/src/modules/users/dto/users-dto.ts
@@ -1,6 +1,19 @@
+import { IsDateString, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class UserDto {
+  @IsNumber()
+  @IsNotEmpty()
   id: number;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   email: string;
+
+  @IsDateString()
+  @IsNotEmpty()
   birthday: Date;
 }
--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # nest-ddr
+
 Curso nest
 
 # Swagger
+
 ```sh
 npm install --save @nestjs/swagger
+```
+
+# Validación
+
+```sh
+npm i class-validator
 ```
