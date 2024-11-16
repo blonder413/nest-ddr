de29afb6bd7d02b8ada396a0f0dc4b6438bb537a
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-11-16 17:46:37 -0500

    api users: documentar dto

--- 02-users/src/modules/users/dto/users-dto.ts
+++ 02-users/src/modules/users/dto/users-dto.ts
@@ -1,19 +1,44 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsDate, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class UserDto {
+  @ApiProperty({
+    name: 'id',
+    type: Number,
+    description: 'Identificador del usuario',
+    required: true,
+  })
   @IsNumber()
   @IsNotEmpty()
   id: number;
 
+  @ApiProperty({
+    name: 'name',
+    type: String,
+    description: 'Nombre del usuario',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    name: 'email',
+    type: String,
+    description: 'Correo del usuario',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    name: 'birthDate',
+    type: Date,
+    description: 'Fecha de nacimiento del usuario',
+    required: true,
+  })
   @IsDate()
   @IsNotEmpty()
   @Type(() => Date)
