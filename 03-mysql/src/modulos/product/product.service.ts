7faeac32587936d42b4a76cf04a2afd61a61c6e1
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-11-17 14:45:32 -0500

    inyectar repositorio en product service

--- 03-mysql/src/modulos/product/product.service.ts
+++ 03-mysql/src/modulos/product/product.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entity/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductService {
-  constructor() {}
+  constructor(
+    @InjectRepository(Product) private productRepository: Repository<Product>,
+  ) {}
 }
