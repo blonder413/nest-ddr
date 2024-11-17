925c5c5a0a240f8ac4d51e6d9d97bc238618786a
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-11-17 16:05:36 -0500

    mysql: insertar registro

--- 03-mysql/src/modulos/product/product.controller.ts
+++ 03-mysql/src/modulos/product/product.controller.ts
@@ -1,9 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ProductService } from './product.service';
+import { ProductDto } from './dto/product-dto';
 
 @ApiTags('productos')
 @Controller('api/v1/products')
 export class ProductController {
   constructor(private productService: ProductService) {}
+
+  @Post()
+  createProduct(@Body() product: ProductDto) {
+    return this.productService.createProduct(product);
+  }
 }
--- 03-mysql/src/modulos/product/product.service.ts
+++ 03-mysql/src/modulos/product/product.service.ts
@@ -2,10 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entity/product.entity';
 import { Repository } from 'typeorm';
+import { ProductDto } from './dto/product-dto';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectRepository(Product) private productRepository: Repository<Product>,
   ) {}
+
+  async createProduct(product: ProductDto) {
+    return await this.productRepository.save(product);
+  }
 }
