7c2647c394d546e300bf4419dd2c5db205055abc
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-11-19 09:21:20 -0500

    swagger: documentar api de productos

--- 03-mysql/src/modulos/product/product.controller.ts
+++ 03-mysql/src/modulos/product/product.controller.ts
@@ -8,7 +8,7 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { ProductService } from './product.service';
 import { ProductDto } from './dto/product-dto';
 import { StockDto } from './dto/stock-dto';
@@ -18,51 +18,172 @@ import { StockDto } from './dto/stock-dto';
 export class ProductController {
   constructor(private productService: ProductService) {}
 
+  @ApiBody({
+    description: 'Crea un producto mediante un ProductDto',
+    type: ProductDto,
+    examples: {
+      ejemplo1: {
+        value: {
+          "name": "producto ejemplo",
+          "price": 810,
+          "stock": 13
+        },
+      },
+    },
+  })
+  @ApiOperation({
+    description: 'crea un producto',
+  })
   @Post()
   createProduct(@Body() product: ProductDto) {
     return this.productService.createProduct(product);
   }
 
+
+  @ApiOperation({
+    description: "lista todos los productos borrados"
+  })
+  @ApiParam({
+    description: "id del producto",
+    name: "id",
+    required: true,
+    type: Number
+  })
   @Get('/deleted')
   getDeleted() {
     return this.productService.findDeleted();
   }
 
+
+  @ApiOperation({
+    description: "lista producto buscando por id"
+  })
+  @ApiParam({
+    description: "id del producto",
+    name: "id",
+    required: true,
+    type: Number
+  })
   @Get('/:id')
   getProductById(@Param('id') id: number) {
     return this.productService.findProduct(id);
   }
 
+
+  @ApiOperation({
+    description: "lista todos los productos no borrados"
+  })
   @Get()
   getProducts() {
     return this.productService.findAll();
   }
 
+
+  @ApiBody({
+    description: 'Actualiza un producto mediante un ProductDto, si no se envía el id se crea',
+    type: ProductDto,
+    examples: {
+      ejemplo1: {
+        value: {
+          "id": 1,
+          "name": "producto actualizado",
+          "price": 810,
+          "stock": 13
+        },
+      },
+      ejemplo2: {
+        value: {
+          "name": "producto nuevo",
+          "price": 810,
+          "stock": 13
+        },
+      },
+    },
+  })
+  @ApiOperation({
+    description: "actualiza un producto, si no existe se crea"
+  })
   @Put()
   updateProduct(@Body() product: ProductDto) {
     return this.productService.updateProduct(product);
   }
 
+
+  @ApiParam({
+    description: "id del producto",
+    name: "id",
+    required: true,
+    type: Number
+  })
+  @ApiOperation({
+    description: "Borra un producto (soft delete)"
+  })
   @Delete('/:id')
   deleteProduct(@Param('id') id: number) {
     return this.productService.softDeleteProduct(id);
   }
 
+
+  @ApiOperation({
+    description: "Recupera un producto borrado"
+  })
   @Patch('/restore/:id')
   restoreProduct(@Param('id') id: number) {
     return this.productService.restoreProduct(id);
   }
 
+
+  @ApiBody({
+    description: 'Actualiza el stock de un producto mediante un StockDto',
+    type: StockDto,
+    examples: {
+      ejemplo1: {
+        value: {
+          "id": 4,
+	        "stock": 10
+        },
+      }
+    },
+  })
+  @ApiOperation({
+    description: "Actualiza el stock de un producto"
+  })
   @Patch('/stock')
   updateStock(@Body() stock: StockDto) {
     return this.productService.updateStock(stock);
   }
 
+
+  @ApiBody({
+    description: 'Incrementa el stock de un producto mediante un StockDto',
+    type: StockDto,
+    examples: {
+      ejemplo1: {
+        value: {
+          "id": 4,
+	        "stock": 10
+        },
+      }
+    },
+  })
   @Patch('/increment-stock')
   incrementStock(@Body() stock: StockDto) {
     return this.productService.incrementStock(stock);
   }
 
+
+  @ApiBody({
+    description: 'Decrementa el stock de un producto mediante un StockDto',
+    type: StockDto,
+    examples: {
+      ejemplo1: {
+        value: {
+          "id": 4,
+	        "stock": 10
+        },
+      }
+    },
+  })
   @Patch('/decrement-stock')
   decrementStock(@Body() stock: StockDto) {
     return this.productService.decrementStock(stock);
