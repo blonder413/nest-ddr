d691ff32e59221dfea69c0903c158ac186e071ea
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-11-20 15:20:21 -0500

    swagger: documentar dto

--- 03-mysql/src/modulos/product/dto/product-dto.ts
+++ 03-mysql/src/modulos/product/dto/product-dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
     IsBoolean,
   IsNotEmpty,
@@ -8,25 +9,57 @@ import {
 } from 'class-validator';
 
 export class ProductDto {
+  
+  
+  @ApiProperty({
+    name: "id",
+    required: false,
+    description: "id del producto",
+    type: Number
+  })
   @IsOptional()
   @IsNumber()
   @IsPositive()
   id?: number;
 
+  @ApiProperty({
+    name: "name",
+    required: true,
+    description: "nombre del producto",
+    type: String
+  })
   @IsString()
   @IsNotEmpty()
   name?: string;
 
+  @ApiProperty({
+    name: "price",
+    required: true,
+    description: "precio del producto",
+    type: Number
+  })
   @IsNotEmpty()
   @IsNumber()
   @IsPositive()
   price?: number;
 
+  @ApiProperty({
+    name: "stock",
+    required: true,
+    description: "stock del producto",
+    type: Number
+  })
   @IsNotEmpty()
   @IsNumber()
   @IsPositive()
   stock: number;
 
+  @ApiProperty({
+    name: "deleted",
+    required: false,
+    description: "indica si el producto está borrado",
+    type: Boolean
+  })
   @IsOptional()
   @IsBoolean()
   deleted?: boolean;
