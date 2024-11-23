bc1ff518f9575933ecc789fe4f7497d82c3d7bdf
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-11-23 17:04:29 -0500

    mongodb: se inyecta servicio conexión en los permisos

--- 05-mongo-db/src/modules/permissions/permissions.service.ts
+++ 05-mongo-db/src/modules/permissions/permissions.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Permission } from './schemas/permission.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class PermissionsService {}
+export class PermissionsService {
+  constructor(
+    @InjectModel(Permission.name) private permissionModel: Model<Permission>,
+  ) {}
+}
