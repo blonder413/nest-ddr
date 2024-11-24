a01222309c8386fd6153a4a0210b80914879c575
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-11-24 18:26:30 -0500

    mongodb: creación de usuario dto

---
+++ 05-mongo-db/src/modules/users/dto/user-dto.ts
@@ -0,0 +1,55 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsDate,
+  IsEmail,
+  IsNotEmpty,
+  IsObject,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { RoleDto } from 'src/modules/roles/dto/role-dto';
+
+export class UserDto {
+  @ApiProperty({
+    name: 'name',
+    type: String,
+    required: true,
+    description: 'Nombre del usuario',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name!: string;
+
+  @ApiProperty({
+    name: 'email',
+    type: String,
+    required: true,
+    description: 'Email del usuario',
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email!: string;
+
+  @ApiProperty({
+    name: 'birthday',
+    type: Date,
+    required: true,
+    description: 'Fecha de nacimiento del usuario',
+  })
+  @IsDate()
+  @IsNotEmpty()
+  @Type(() => Date)
+  birthday!: Date;
+
+  @ApiProperty({
+    name: 'role',
+    type: RoleDto,
+    required: false,
+    description: 'Rol del usuario',
+  })
+  @IsOptional()
+  @IsObject()
+  @Type(() => RoleDto)
+  role?: RoleDto;
+}
