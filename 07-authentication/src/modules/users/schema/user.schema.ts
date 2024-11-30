2805a2ff7e09200ef82de14f7bb4b51ed977ce45
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-11-30 17:40:33 -0500

    authentication: user schema

---
+++ 07-authentication/src/modules/users/schema/user.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class User {
+  @Prop({
+    type: String,
+    unique: true,
+    trim: true,
+    lowecase: true,
+    required: true,
+  })
+  email: string;
+
+  @Prop({ type: String, required: true })
+  password: string;
+}
+
+export const userSchema = SchemaFactory.createForClass(User);
