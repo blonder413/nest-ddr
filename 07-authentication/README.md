5f102f4d2f5fb1baf6c00c2e2165ecb2668e1330
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-11-30 16:27:57 -0500

    authentication: proyecto inicial

---
+++ 07-authentication/README.md
@@ -0,0 +1,23 @@
+# Validación
+
+```sh
+npm i class-validator
+```
+# Validation Pipe
+
+```sh
+npm i class-transformer
+```
+
+# Swagger
+
+```sh
+npm i --save @nestjs/swagger
+npm i swagger-ui-express
+```
+
+# [MongoDB](https://docs.nestjs.com/techniques/mongodb)
+
+```sh
+npm i @nestjs/mongoose mongoose
+```
---
+++ 07-authentication/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Authentication')
+    .setDescription('Ejemplo authentication mongodb')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
