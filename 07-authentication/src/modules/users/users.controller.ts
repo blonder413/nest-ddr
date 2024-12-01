75d8d2c74d8c3772926da7a6ff6653fe043b2de8
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-01 11:38:17 -0500

    authentication: insertar usuario

--- 07-authentication/src/modules/users/users.controller.ts
+++ 07-authentication/src/modules/users/users.controller.ts
@@ -1,9 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
+import { UserDto } from './dto/user-dto';
 
 @Controller('/api/v1/users')
 @ApiTags('Users')
 export class UsersController {
   constructor(private userService: UsersService) {}
+
+  @Post()
+  createUser(@Body() user: UserDto) {
+    return this.userService.createUser(user);
+  }
 }
--- 07-authentication/src/modules/users/users.service.ts
+++ 07-authentication/src/modules/users/users.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schema/user.schema';
 import { Model } from 'mongoose';
+import { UserDto } from './dto/user-dto';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async createUser(user: UserDto) {
+    const userExists = await this.userModel.findOne({ email: user.email });
+
+    if (userExists) {
+      throw new ConflictException(
+        `El usuario con email ${user.email} ya existe`,
+      );
+    }
+
+    const u = new this.userModel(user);
+    await u.save();
+    u.password = undefined;
+    return u;
+  }
 }
