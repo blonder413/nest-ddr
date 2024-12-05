3578cd0fd9a7c4cff0fc464bfd7102846c9ee3b7
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-05 18:06:47 -0500

    jwt: mostrar usuario logueado

--- 07-authentication/src/modules/auth/auth.controller.ts
+++ 07-authentication/src/modules/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials-dto';
 import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('/api/v1/auth')
 @ApiTags('Auth')
@@ -11,5 +12,11 @@ export class AuthController {
   @Post('/login')
   login(@Body() authCredentials: AuthCredentialsDto) {
     return this.authService.login(authCredentials);
+  }
+
+  @Get('data-user')
+  @UseGuards(AuthGuard('jwt'))
+  dataUser(@Req() request) {
+    return request.user;
   }
 }
