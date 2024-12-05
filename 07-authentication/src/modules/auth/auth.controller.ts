54e7dd517c3797451d584f1111c23829e9024861
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-05 17:52:43 -0500

    jwt: login

--- 07-authentication/src/modules/auth/auth.controller.ts
+++ 07-authentication/src/modules/auth/auth.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials-dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('/api/v1/auth')
+@ApiTags('Auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('/login')
+  login(@Body() authCredentials: AuthCredentialsDto) {
+    return this.authService.login(authCredentials);
+  }
 }
--- 07-authentication/src/modules/auth/auth.service.ts
+++ 07-authentication/src/modules/auth/auth.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { AuthCredentialsDto } from './dto/auth-credentials-dto';
 import * as bcrypt from 'bcrypt';
+import { JwtPayload } from './dto/jwt-payload';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(userCredencials: AuthCredentialsDto) {
     const user = await this.userService.getUserByEmail(userCredencials.email);
@@ -19,5 +24,17 @@ export class AuthService {
       }
     }
     return null;
+  }
+
+  async login(authCredentials: AuthCredentialsDto) {
+    const user = await this.validateUser(authCredentials);
+    if (!user) {
+      throw new UnauthorizedException('Credenciales inválidas');
+    }
+    const payload: JwtPayload = {
+      email: user.email,
+    };
+
+    return { accessToken: this.jwtService.sign(payload) };
   }
 }
