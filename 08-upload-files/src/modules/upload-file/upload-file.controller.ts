1d21b862393fa80c710e18d57f2f152baae848ce
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-06 16:04:29 -0500

    swagger: documentar upload

--- 08-upload-files/src/modules/upload-file/upload-file.controller.ts
+++ 08-upload-files/src/modules/upload-file/upload-file.controller.ts
@@ -7,7 +7,7 @@ import {
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UploadFileService } from './upload-file.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 
@@ -18,11 +18,16 @@ export class UploadFileController {
 
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
+  @ApiOperation({ description: 'Sube un archivo' })
+  @ApiResponse({ status: 201, description: 'Se ha subido correctamente' })
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     return this.uploadFileService.uploadFile(file);
   }
 
   @Get('download')
+  @ApiOperation({ description: 'Descarga un archivo' })
+  @ApiResponse({ status: 200, description: 'Se ha descargado correctamente' })
+  @ApiResponse({ status: 409, description: 'No existe el archivo' })
   downloadFile(@Response() res, @Body() body: any) {
     return this.uploadFileService.download(res, body.filename);
   }
