9ad1a380fe57dac5accf8e996abd24085784e4c4
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-06 14:48:07 -0500

    upload: subir un archivo

--- 08-upload-files/src/modules/upload-file/upload-file.controller.ts
+++ 08-upload-files/src/modules/upload-file/upload-file.controller.ts
@@ -1,9 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UploadFileService } from './upload-file.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('upload-file')
 @ApiTags('Uploads')
 export class UploadFileController {
   constructor(private uploadFileService: UploadFileService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.uploadFileService.uploadFile(file);
+  }
 }
--- 08-upload-files/src/modules/upload-file/upload-file.service.ts
+++ 08-upload-files/src/modules/upload-file/upload-file.service.ts
@@ -3,4 +3,11 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UploadFileService {
   constructor() {}
+
+  uploadFile(file: Express.Multer.File) {
+    if (file) {
+      return { originalName: file.originalname, filename: file.filename };
+    }
+    return null;
+  }
 }
