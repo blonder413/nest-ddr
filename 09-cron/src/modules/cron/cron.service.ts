cc0594dd1b9f29b6285651541f0ab4c0160d0197
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-06 16:52:35 -0500

    cron: creando un cron

--- 09-cron/src/modules/cron/cron.service.ts
+++ 09-cron/src/modules/cron/cron.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { SchedulerRegistry } from '@nestjs/schedule';
+import { Cron, SchedulerRegistry } from '@nestjs/schedule';
 
 @Injectable()
 export class CronService {
   constructor(private schedulerRegistry: SchedulerRegistry) {}
+
+  @Cron('*/10 * * * * *', { name: 'cron1' })
+  cron1() {
+    console.log('Cron1: se ejecuta cada 10 segundos');
+  }
+
+  @Cron('*/30 * * * * *', { name: 'cron2' })
+  cron2() {
+    console.log('Cron2: se ejecuta cada 30 segundos');
+  }
+
+  @Cron('* * * * *', { name: 'cron3' })
+  cron3() {
+    console.log('Cron3: se ejecuta cada minuto');
+  }
 }
