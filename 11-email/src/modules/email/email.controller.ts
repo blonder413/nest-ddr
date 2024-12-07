cdd5efb31fdef1c8a50d18ea2ae042afeb2f93c5
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-07 18:14:44 -0500

    swagger: documentar endpoint de envío de correo

--- 11-email/src/modules/email/email.controller.ts
+++ 11-email/src/modules/email/email.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { EmailService } from './email.service';
 import { MessageDto } from './dto/message-dto';
 
@@ -9,6 +9,29 @@ export class EmailController {
   constructor(private emailService: EmailService) {}
 
   @Post('send-email')
+  @ApiOperation({ description: 'Envía un email' })
+  @ApiBody({
+    description: 'Envía un email usando un MessageDto',
+    type: MessageDto,
+    examples: {
+      ejemplo1: {
+        value: {
+          subject: 'Prueba nestjs',
+          body: '<h2>Prueba desde nestjs</h2>',
+          receivers: [
+            {
+              email: 'jvalentine@bsaa.org',
+            },
+            {
+              email: 'credfield@terrasave.org',
+            },
+          ],
+        },
+      },
+    },
+  })
+  @ApiResponse({ status: 201, description: 'Correo enviado correctamente' })
+  @ApiResponse({ status: 500, description: 'Error interno del servidor' })
   sendEmail(@Body() message: MessageDto) {
     return this.emailService.sendEmail(message);
   }
