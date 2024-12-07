59b137952d3e2081bc93d1392aae8049a2086053
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-07 14:16:09 -0500

    log: generar archivo de log por día

--- 10-logger/src/modules/logger/logger.service.ts
+++ 10-logger/src/modules/logger/logger.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { createLogger, format, Logger, transports } from 'winston';
+import 'winston-daily-rotate-file';
 
 @Injectable()
 export class LoggerService {
@@ -22,25 +23,55 @@ export class LoggerService {
     this.loggerInfo = createLogger({
       level: 'info',
       format: format.combine(dateFormat, textFormat),
-      transports: [new transports.File({ filename: 'logs/info/info.log' })],
+      /** new transports.File({ filename: 'logs/info/info.log' }), */
+      transports: [
+        new transports.DailyRotateFile({
+          filename: 'logs/info/info-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
+          maxFiles: '7d',
+          /** zippedArchive: true, Lo que se comprima no se borra */
+        }),
+      ],
     });
 
     this.loggerError = createLogger({
       level: 'error',
       format: format.combine(dateFormat, textFormat),
-      transports: [new transports.File({ filename: 'logs/error/error.log' })],
+      /** new transports.File({ filename: 'logs/error/error.log' }), */
+      transports: [
+        new transports.DailyRotateFile({
+          filename: 'logs/error/error-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
+          maxFiles: '7d',
+          /** zippedArchive: true, Lo que se comprima no se borra */
+        }),
+      ],
     });
 
     this.loggerWarn = createLogger({
       level: 'warn',
       format: format.combine(dateFormat, textFormat),
-      transports: [new transports.File({ filename: 'logs/warn/warn.log' })],
+      /** new transports.File({ filename: 'logs/warn/warn.log' }), */
+      transports: [
+        new transports.DailyRotateFile({
+          filename: 'logs/warn/warn-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
+          maxFiles: '7d',
+          /** zippedArchive: true, Lo que se comprima no se borra */
+        }),
+      ],
     });
 
     this.loggerAll = createLogger({
       format: format.combine(dateFormat, textFormat),
       transports: [
-        new transports.File({ filename: 'logs/all/all.log' }),
+        /** new transports.File({ filename: 'logs/all/all.log' }), */
+        new transports.DailyRotateFile({
+          filename: 'logs/all/all-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
+          maxFiles: '7d',
+          /** zippedArchive: true, Lo que se comprima no se borra */
+        }),
         new transports.Console(),
       ],
     });
