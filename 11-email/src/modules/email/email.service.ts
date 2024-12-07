5716724b7a15090b902337cf285ea317de56ee25
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-07 17:58:37 -0500

    mail: enviar correos con nodemailer

--- 11-email/src/modules/email/email.service.ts
+++ 11-email/src/modules/email/email.service.ts
@@ -6,11 +6,36 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class EmailService {
-  constructor(@Inject('CONFIG_OPTIONS') private options: EmailConfig, private configService:ConfigService) {
+  constructor(
+    @Inject('CONFIG_OPTIONS') private options: EmailConfig,
+    private configService: ConfigService,
+  ) {
     /** console.log(this.configService.get('config.from')); */
     /** console.log(this.options); */
-    
   }
 
-  sendEmail(message: MessageDto) {}
+  sendEmail(message: MessageDto) {
+    try {
+      const transporter = nodemailer.createTransport({
+        service: this.options.service,
+        auth: {
+          user: this.options.from,
+          pass: this.options.password,
+        },
+      });
+
+      const to = message.receivers.map((e) => e.email);
+
+      const mailOptions = {
+        from: this.options.from,
+        to,
+        subject: message.subject,
+        html: message.body,
+      };
+      return transporter.sendEmail(mailOptions);
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
 }
