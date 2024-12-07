b33e4eb2d5f461466d32d8d2c3b282d108ac945e
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-07 16:10:59 -0500

    email: endpoint para enviar email

--- 11-email/src/modules/email/email.controller.ts
+++ 11-email/src/modules/email/email.controller.ts
@@ -1,9 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { EmailService } from './email.service';
+import { MessageDto } from './dto/message-dto';
 
 @Controller('api/v1/email')
 @ApiTags('Emails')
 export class EmailController {
   constructor(private emailService: EmailService) {}
+
+  @Post('send-email')
+  sendEmail(@Body() message: MessageDto) {
+    return this.emailService.sendEmail(message);
+  }
 }
--- 11-email/src/modules/email/email.service.ts
+++ 11-email/src/modules/email/email.service.ts
@@ -1,8 +1,11 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { EmailConfig } from './email-config';
 import * as nodemailer from 'nodemailer';
+import { MessageDto } from './dto/message-dto';
 
 @Injectable()
 export class EmailService {
   constructor(@Inject('CONFIG_OPTIONS') private options: EmailConfig) {}
+
+  sendEmail(message: MessageDto) {}
 }
