cfbd043904d603b550762078d44acaffd9fe78c9
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-07 10:12:52 -0500

    log: creación de servicio

--- 10-logger/src/modules/logger/logger.service.ts
+++ 10-logger/src/modules/logger/logger.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
-export class LoggerService {}
+export class LoggerService {
+  private loggerInfo: Logger;
+  private loggerError: Logger;
+  private loggerWarn: Logger;
+  private loggerAll: Logger;
+
+  constructor() {}
+
+  createLogger() {}
+}
