653b7e0fdbbb0e310ac5cf9cf742306f8316a016
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-08 09:03:20 -0500

    microservicios: conectar microservicios

--- 12-microservicios/12-microservicios-backend-2/src/modules/microservice-connection/microservice-connection.service.ts
+++ 12-microservicios/12-microservicios-backend-2/src/modules/microservice-connection/microservice-connection.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
 
 @Injectable()
-export class MicroserviceConnectionService {}
+export class MicroserviceConnectionService {
+    private client: ClientProxy;
+  constructor() {
+    this.client = ClientProxyFactory.create({
+      transport: Transport.TCP,
+      options: { host: '0.0.0.0', port: 3032 },
+    });
+  }
+
+  getClient() {
+    return this.client;
+  }
+}
