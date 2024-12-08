158e60d32a00a4873a9a0186f4df8495455aafa5
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-08 08:56:12 -0500

    microservicios: crear microservicios

--- 12-microservicios/12-microservicios-backend-1/src/main.ts
+++ 12-microservicios/12-microservicios-backend-1/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: { host: '0.0.0.0', port: 3032 },
+  });
+  await app.startAllMicroservices();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- 12-microservicios/12-microservicios-backend-2/src/main.ts
+++ 12-microservicios/12-microservicios-backend-2/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: { host: '0.0.0.0', port: 3030 },
+  });
+  await app.startAllMicroservices();
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
