a22643ba5b1fed3d3d5e2fa59178905be49ccab5
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-08 14:14:46 -0500

    mqtt: crear conexión con mqtt parte 2

--- 13-mqtt/src/modules/mqtt-subscriber/mqtt-subscriber.module.ts
+++ 13-mqtt/src/modules/mqtt-subscriber/mqtt-subscriber.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { MqttSubscriberController } from './mqtt-subscriber.controller';
 import { MqttSubscriberService } from './mqtt-subscriber.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { connectionMQTT } from 'src/constants/constants';
 
 @Module({
   controllers: [MqttSubscriberController],
-  providers: [MqttSubscriberService]
+  imports: [
+    ClientsModule.register([
+      {
+        transport: Transport.MQTT,
+        name: connectionMQTT.clientID,
+        options: {
+          url: `mqtt://${connectionMQTT.broker.host}:${connectionMQTT.broker.port}`,
+        },
+      },
+    ]),
+  ],
+  providers: [MqttSubscriberService],
 })
 export class MqttSubscriberModule {}
--- 13-mqtt/src/modules/mqtt-subscriber/mqtt-subscriber.service.ts
+++ 13-mqtt/src/modules/mqtt-subscriber/mqtt-subscriber.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { connectionMQTT } from 'src/constants/constants';
 
 @Injectable()
-export class MqttSubscriberService {}
+export class MqttSubscriberService {
+  constructor(@Inject(connectionMQTT.clientID) private client: ClientProxy) {}
+}
